041c95a8b807c0d983c4e1301c2d3c2ce7000f78
Heidi Kuang <kuangheidi27@gmail.com>
2024-11-11 15:58:26 -0600

    um idk

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -101,8 +101,10 @@ const SignupForm = () => {
 
   return (
     <Form {...form}>
+      <img src="/assets/images/logo.png" alt="logo" className="w-20"/>
+
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo"/>
+        
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">To use Promenade, enter your details</p>
